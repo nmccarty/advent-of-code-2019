06848c7b408b147d653bacf5fd421cf717fccd65
Nathan McCarty <nathan@mccarty.io>
2019-12-19 11:31:43 -0500

    Solve day 1 part two

--- advent1/src/Main.hs
+++ advent1/src/Main.hs
@@ -3,5 +3,11 @@ module Main where
 fuelNeeded :: Int -> Int
 fuelNeeded mass = mass `div` 3 - 2
 
+totalNeeded :: Int -> Int
+totalNeeded mass = if fuel <= 0
+                   then 0
+                   else fuel + totalNeeded fuel
+  where fuel = fuelNeeded mass
+
 main :: IO ()
-main = interact (show . sum . map (fuelNeeded . read) . lines)
+main = interact (show . sum . map (totalNeeded . read) . lines)
